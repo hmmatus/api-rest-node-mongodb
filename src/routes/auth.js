9f30a133c84189ee0547bcda7318c9b98ae71ce9
Hector Matus <{ID}+{username}@users.noreply.github.com>
2023-12-19 18:37:55 -0600

    Added config library

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,25 +2,44 @@ const express = require("express");
 const route = express.Router();
 const User = require("../models/user_model");
 const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
+const config = require('config')
 
-route.post('/', (req, res) => {
-  User.findOne({email: req.body.email})
-  .then((data) => {
-    const passwordValid = bcrypt.compareSync(req.body.password, data.password);
-    if (data && passwordValid) {
-      res.json(data);
-    } else {
-      res.status(400).json({
-        error: 'ok',
-        msg: 'Incorrect user or password',
-      })
-    }
-  }).catch((error) => {
-    res.status(400).json({
-      error: 'ok',
-      msg: `Error in service ${error}`
+route.post("/", (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then((data) => {
+      if (data) {
+        const passwordValid = bcrypt.compareSync(
+          req.body.password,
+          data.password
+        );
+        if (!passwordValid)
+          return res.status(400).json({
+            error: "ok",
+            msg: "Incorrect user or password",
+          });
+        const jwtToken = jwt.sign({data: {_id: data._id, name: data.name, email: data.email}}, config.get('configToken.SEED'), {expiresIn: config.get('configToken.expiration')});
+        res.json({
+          user: {
+            _id: data._id,
+            name: data.name,
+            email: data.email,
+          },
+          jwt: jwtToken
+        });
+      } else {
+        res.status(400).json({
+          error: "ok",
+          msg: "Incorrect user or password",
+        });
+      }
     })
-  })
-})
+    .catch((error) => {
+      res.status(400).json({
+        error: "ok",
+        msg: `Error in service ${error}`,
+      });
+    });
+});
 
 module.exports = route;
